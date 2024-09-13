7e2785a0ef0826027ab02ba356277f77f60475f7
mananbansal77 <158815887+mananbansal77@users.noreply.github.com>
2024-09-13 14:00:55 +0000

    variable learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail ="bansalmanan007@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+// accountId =2 not allowed
+// accountEmail="kruh"
+console.log(accountId);
+// console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
